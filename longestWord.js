93c255a296bcf10b53fa465e109ad11df4936d60
Leah <rhodycodes401@gmail.com>
2018-01-04 04:35:43 +0000

    Next Commit

---
+++ longestWord.js
@@ -0,0 +1,16 @@
+
+function findLongestWord(str) {
+  var arraySplitString =  [];
+  var longest = 0; 
+  
+  arraySplitString = str.split(" ");
+  console.log(arraySplitString);
+  for (var i = 0; i < arraySplitString.length; i++)
+    if (arraySplitString[i].length > longest) {
+      longest = arraySplitString[i].length;
+      return longest;
+    } 
+   
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
